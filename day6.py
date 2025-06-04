9a5515276e12c5675df95231c118dae67aad1c77
madhukiranrao <mkagamah@gitam.in>
2025-06-04 12:26:44 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,30 @@
+# Day 6: Linear Regression - Predict House Prices
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+data = pd.read_csv("datasets/boston_housing.csv")
+
+X = data[['rm', 'lstat', 'ptratio']]
+y = data['medv']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+print("Mean Squared Error:", mse)
+
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Prices")
+plt.ylabel("Predicted Prices")
+plt.title("Actual vs Predicted House Prices")
+plt.grid(True)
+plt.show()
